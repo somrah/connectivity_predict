d149f1d1a8979e0c8e86c6ef87e18558ab26a96f
lebbe <lebbe@macbook-pro-de-lebbe.home>
2020-09-16 23:47:16 +0200

    Update first_script.py

--- first_script.py
+++ first_script.py
@@ -4,13 +4,28 @@ compact features for prediction.
 
 Author: Bertrand Thirion, 2020
 """
-
+#import libraries
 import pandas as pd
 import numpy as np
+from scipy import stats
+from scipy.stats import mannwhitneyu
+
+import matplotlib.pyplot as plt
+from math import pi
+
+
+from sklearn.svm import SVC
+from sklearn.model_selection import (cross_val_score, KFold, GridSearchCV,
+                                     train_test_split)
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import tree
+from sklearn.tree import (DecisionTreeClassifier,export_text)
+
 
 # replace with wherever you put that file
 csv_file ='/Users/lebbe/Downloads/re/all_connections.csv'
-csv_file_behavior = '/Users/lebbe/Downloads/behavior.csv'
+csv_file_behavior = '/Users/lebbe/Downloads/behavior2.csv'
 
 #directory where figures are saved
 mypath = '/Users/lebbe/Downloads/'
@@ -19,6 +34,7 @@ mypath = '/Users/lebbe/Downloads/'
 all_connections = pd.read_csv(csv_file, index_col=0)
 #this is behavioral data ( 1 indicates the presence of perseveration)
 behavior = pd.read_csv(csv_file_behavior, index_col=0)
+
 # get the pathway names
 connection_ids = all_connections.columns[2:] # discard subjectID and TrackID
 unique_ids = np.unique([c.split('_')[0] + '_'  + c.split('_')[1]
@@ -66,39 +82,39 @@ average_connectivity['subjectID'] = unique_subjects
 # 1) for each subject make a sum of ratios obtained in each HCP connectome
 # 2) transform ratios with sigmoid function
 
-unique_subjects = all_connections['subjectID'].unique()
-ratios_connectivity_bis_HCP = {}
-sumratios_connectivity_bis_subject = {}
-
-for id_ in unique_ids:
-    ratios_connectivity_bis_HCP[id_] = aggregated_connectivity[id_] / (
-        1. + aggregated_connectivity[id_ + '_total'])
-ratios_connectivity_bis_HCP = pd.DataFrame(ratios_connectivity_bis_HCP)
-ratios_connectivity_bis_HCP['subjectID'] = all_connections['subjectID']
-
-for subject in unique_subjects:
-    x = ratios_connectivity_bis_HCP[
-        ratios_connectivity_bis_HCP.subjectID == subject]
-    sumratios_connectivity_bis_subject[subject] = ratios_connectivity_bis_HCP[
-        ratios_connectivity_bis_HCP.subjectID == subject].sum(0)
-sumratios_connectivity_bis_subject = pd.DataFrame(
-    sumratios_connectivity_bis_subject).T
-sumratios_connectivity_bis_subject_transformed = 1 / (1 + np.exp(np.asarray(
-    - sumratios_connectivity_bis_subject.drop(['subjectID'], axis=1),
-    dtype=float)))
-sumratios_connectivity_bis_subject_transformed = pd.DataFrame(
-    sumratios_connectivity_bis_subject_transformed)
-sumratios_connectivity_bis_subject_transformed['subjectID'] = unique_subjects
-ANTS = [subject for subject in unique_subjects if subject.endswith('ANTS')]
-ANTS_sumratios_connectivity_bis_subject_transformed = (
-    sumratios_connectivity_bis_subject_transformed[
-        sumratios_connectivity_bis_subject_transformed.subjectID.isin(ANTS)])
+#unique_subjects = all_connections['subjectID'].unique()
+#ratios_connectivity_bis_HCP = {}
+#sumratios_connectivity_bis_subject = {}
+#
+#for id_ in unique_ids:
+#    ratios_connectivity_bis_HCP[id_] = aggregated_connectivity[id_] / (
+#        1. + aggregated_connectivity[id_ + '_total'])
+#ratios_connectivity_bis_HCP = pd.DataFrame(ratios_connectivity_bis_HCP)
+#ratios_connectivity_bis_HCP['subjectID'] = all_connections['subjectID']
+#
+#for subject in unique_subjects:
+#    x = ratios_connectivity_bis_HCP[
+#        ratios_connectivity_bis_HCP.subjectID == subject]
+#    sumratios_connectivity_bis_subject[subject] = ratios_connectivity_bis_HCP[
+#        ratios_connectivity_bis_HCP.subjectID == subject].sum(0)
+#sumratios_connectivity_bis_subject = pd.DataFrame(
+#    sumratios_connectivity_bis_subject).T
+#sumratios_connectivity_bis_subject_transformed = 1 / (1 + np.exp(np.asarray(
+#    - sumratios_connectivity_bis_subject.drop(['subjectID'], axis=1),
+#    dtype=float)))
+#sumratios_connectivity_bis_subject_transformed = pd.DataFrame(
+#    sumratios_connectivity_bis_subject_transformed)
+#sumratios_connectivity_bis_subject_transformed['subjectID'] = unique_subjects
+#ANTS = [subject for subject in unique_subjects if subject.endswith('ANTS')]
+#ANTS_sumratios_connectivity_bis_subject_transformed = (
+#    sumratios_connectivity_bis_subject_transformed[
+#        sumratios_connectivity_bis_subject_transformed.subjectID.isin(ANTS)])
 
 ##############################################################################
 # Keep only ANTS subjects
 
 # ANTS = [subject for subject in unique_subjects if subject.endswith('ANTS')]
-
+ANTS = [subject for subject in unique_subjects if subject.endswith('ANTS')]
 ANTS_connectivity = average_connectivity[
     average_connectivity.subjectID.isin(ANTS)]
 
@@ -110,6 +126,9 @@ FSL_connectivity = average_connectivity[
 ##############################################################################
 # finally compute the  partial/total ratio in each subject
 ANTS_ratio = {}
+ANTS = [subject for subject in unique_subjects if subject.endswith('ANTS')]
+ANTS_connectivity = average_connectivity[
+    average_connectivity.subjectID.isin(ANTS)]
 for id_ in unique_ids:
     ANTS_ratio[id_] = ANTS_connectivity[id_] / (
         1. + ANTS_connectivity[id_ + '_total'])
@@ -135,44 +154,103 @@ FSL_ratio.name = 'FSL_ratio'
 FSL_ratio_transformed = 1 / (1 + np.exp(np.asarray(- FSL_ratio,dtype=float)))
 FSL_ratio_transformed = pd.DataFrame(FSL_ratio_transformed)
 FSL_ratio_transformed.name = 'FSL_ratio_transformed'
+
+
+
 ##############################################################################
-#plot ANTS ratio (corticocortical, corticostriatal, corticothalamic)
-# import plotting libraries and pi
+#LISTS ACCORDING TO BEHAVIOR
+#change name of columns with abbreviations to make radar plots more readable
+ANTS_ratio.columns = ANTS_ratio.columns.str.replace(r"CorticoThalamic","CT")
+ANTS_ratio.columns = ANTS_ratio.columns.str.replace(r"CorticoStriatal","CS")
+ANTS_ratio.columns = ANTS_ratio.columns.str.replace(r"CorticoCortical","CC")
+#remove networks for which data are not reliable
+ANTS_ratio_clean = ANTS_ratio.drop(["CC_1","CC_2","CC_3","CC_11","CS_1","CS_2",
+                                    "CS_5","CS_6","CS_14","CT_1","CT_2","CT_3",
+                                    "CT_4","CT_5","CT_6","CT_9","CT_10","CT_11",
+                                    "CT_13","CT_15"], axis=1)
+    
+#change ANTS_ratio_clean index so that behavior and ANTS_ratio have the same index
+ANTS_ratio_clean.index = behavior.index
+ANTS_ratio_clean = ANTS_ratio_clean
+frames = [ANTS_ratio_clean,behavior]
+ANTS_ratio_clean = pd.concat(frames,axis=1)
 
-import matplotlib.pyplot as plt
-from math import pi
+#defines listes according to behavior of subjects ( cannot make a loop for this)
+A = ANTS_ratio_clean.loc[ANTS_ratio_clean['eVP']== 0]
+A.name = "A"
+B = ANTS_ratio_clean.loc[ANTS_ratio_clean['eVP']== 1]
+B.name = "B"
+C = ANTS_ratio_clean.loc[ANTS_ratio_clean['lVP']== 0]
+C.name = "C"
+D = ANTS_ratio_clean.loc[ANTS_ratio_clean['lVP']== 1]
+D.name = "D"
+E = ANTS_ratio_clean.loc[ANTS_ratio_clean['siVP']== 0]
+E.name = "E"
+F = ANTS_ratio_clean.loc[ANTS_ratio_clean['siVP']== 1]
+F.name = "F"
+G = F.loc[F['eVP']== 0]
+G.name = "G"
+H = F.loc[F['eVP']== 1]
+H.name = "H"
+I = H.loc[F['lVP']== 0]
+I.name = "I"
+J = H.loc[F['lVP']== 1]
+J.name = "J"
 
-# define radar plot function
+AB = A.append(B)
+CD = C.append(D)
+EF = E.append(F)
+IJ = I.append(J)
 
-def make_spider(row, title, liste, color, j, k):
+liste_groupes = [A,B,C,D,E,F,G,H,I,J]
+
+
+for i in liste_groupes : 
+    i.means= i.iloc[:,0:26].mean()
+
+means_AB =[A.means, B.means]
+means_CD = [C.means,D.means]
+means_EF = [E.means,F.means]
+means_GH = [G.means, H.means]
+means_IJ = [I.means,J.means]
+
+#liste_comparaisons = [means_AB,means_CD,means_EF, means_GH, means_IJ]
+#for i in liste_comparaisons
+#    i = pd.concat (i,axis=1)
+#    i = i.reset_index()
+#    i["tvalue"]=""
+#    i["pvalue"]=""
+#
+#    for j in i.index : 
+#        i.loc[j,["tvalue"]]=mannwhitneyu(liste_groupes[i][means_AB.iloc[i,0]],
+#                    B[means_AB.iloc[i,0]])[0]
+#        means_AB.loc[i,["pvalue"]]=mannwhitneyu(A[means_AB.iloc[i,0]],
+#                    B[means_AB.iloc[i,0]])[1]
+#    means_AB["reject"]=multipletests(means_AB["pvalue"], alpha=0.05, 
+#            method='fdr_bh', is_sorted=False, returnsorted=False)[0]
+#    means_AB["p_corr"]=multipletests(means_AB["pvalue"], alpha=0.05, 
+#            method='fdr_bh', is_sorted=False, returnsorted=False)[1]
+#    
+#    i.to_excel(r'/Users/lebbe/Downloads/means_AB_disco.xlsx')
+
+
+##############################################################################
+#PLOT
+def make_spider2(row, title, liste, color, j, k):
     N = len(categories)
     # define the angle for each variable
     angles = [n / float(N) * 2 * pi for n in range(N)]
     angles += angles[:1]   
     # initialize the spider plot
-    ax = plt.subplot(4,10,row+1, polar=True, )
-    # Add behavior
-    ax.text(0.95, 0.06, 'PV early postop =' + str(behavior.values[row,3]),
-            verticalalignment='bottom', horizontalalignment='right',
-            transform=ax.transAxes,
-            color='blue',
-            fontsize=12)
-    ax.text(0.95, 0.005, 'PV late postop =' + str(behavior.values[row,0]),
-            verticalalignment='bottom', horizontalalignment='right',
-            transform=ax.transAxes,
-            color='red', fontsize=12)
-    ax.text(0.95, 0.11, 'PV boucle perop =' + str(behavior.values[row,2]),
-            verticalalignment='bottom', horizontalalignment='right',
-            transform=ax.transAxes,
-            color='purple', fontsize=12)
-
-    # If you want the first axis to be on top:
+    ax = plt.subplot(7,5,row+1, polar=True, )
+#
+#    # If you want the first axis to be on top:
     ax.set_theta_offset(pi / 2)
     ax.set_theta_direction(-1)
 
     # Draw one axe per variable + add labels labels yet
     plt.xticks(angles[:-1],
-               [i.partition('_')[2] for i in categories],
+               [i for i in categories],
                color='black', size=6)
     # Draw ylabels
     ax.set_rlabel_position(0)
@@ -186,25 +264,49 @@ def make_spider(row, title, liste, color, j, k):
     ax.plot(angles[0:-1], values[j:k], color=color, linewidth=2,
             linestyle='solid')
     ax.fill(angles[0:-1], values[j:k], color=color, alpha=0.4)
+   #add information regarding behavior
+        #add information regarding siVP
+    props_red = dict(boxstyle='circle', facecolor='red', alpha=0.5)
+    props_green = dict(boxstyle='circle', facecolor='green', alpha=0.5)
+    props_grey = dict(boxstyle='circle', facecolor='grey', alpha=0.5)
+    bhv=[(26,"lVP",0.90),(28,"siVP",1),(29,"eVP",0.95)]
+    for b,h,v in bhv :
+        if liste.values[row,b]==1 : 
+            
+            ax.text(1, v, h + '+',transform=ax.transAxes,fontsize=2, 
+                    verticalalignment = "top" ,bbox=props_red)
+        elif liste.values[row,b]==0:
+            
+            ax.text(1, v, h +'-',transform=ax.transAxes,fontsize=2, 
+                    verticalalignment = "top" ,bbox=props_green)
+        else :
+            
+            ax.text(1, v, h + '?',transform=ax.transAxes,fontsize=2, 
+                    verticalalignment = "top" ,bbox=props_grey)
+
 
     # Add a title
     plt.title(title, size=11, color='grey', y=1.1)
+    
+    plt.subplots_adjust(left=0.125,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0.1, 
+                    hspace=0.4)
 
-def loop_to_plot(liste, j, k):
+def loop_to_plot2(liste, j, k):
     # initialize the figure
-    my_dpi = 96
-    plt.figure(figsize=(4000/my_dpi, 4000/my_dpi), dpi=my_dpi)
-     
+    my_dpi = 300
+    plt.figure(figsize=(7000/my_dpi, 7000/my_dpi), dpi=my_dpi)
     # Create a color palette:
     my_palette = plt.cm.get_cmap("Set2", len(liste.index))
      
     # Give a name to big figure
-    plt.gcf().text(0.9, 0.9, 
-                   str(categories[0].partition('_')[0]) + '_' 
-                    + liste.name, fontsize=40)
+    plt.gcf().text(0.9, 0.9, liste.name[1:], fontsize=40)
     # Loop to plot
     for row in range(0,len(liste.index)):
-        make_spider(row=row, liste = liste, title=liste.index[row], 
+        make_spider2(row=row, liste = liste, title=liste.index[row].split(' ')[0], 
                     color=my_palette(row), j=j, k=k)
     # save figure 
     plt.savefig(mypath + str(categories[0].partition('_')[0]) + '_' 
@@ -212,46 +314,27 @@ def loop_to_plot(liste, j, k):
             facecolor='w', edgecolor='w', orientation='landscape',
             papertype=None, format=None,transparent=False, bbox_inches=None, 
             pad_inches=0.1, frameon=None, metadata=None)
+# Figure
 
-# Create one figure with all individuals for each level 
-# (CorticoCortical, CorticoStriatal, CorticoThalamic)
-
-# First figure : ANTS CorticoCortical (available in dataset =17)
-j = 0
-k = 17
-liste=ANTS_ratio
-categories = list(liste)[j:k] 
-loop_to_plot (liste=liste, j=j, k=k)
-# Second figure ANTS CorticoStriatal (available in dataset n=15)
-j = 17
-k = 32
-categories = list(liste)[j:k] 
-loop_to_plot (liste=liste, j=j, k=k) 
-# Third figure ANTS CorticoThalamic networks (available in dataset n=14)
-j = 32
-k = 46
-liste=ANTS_ratio
-categories = list(liste)[j:k] 
-loop_to_plot (liste=liste, j=j, k=k)
-
-
+for letter in liste_groupes : 
+    #define all columns with disconnection ratios in dataframe
+    j = 0 
+    k = 26
+    liste=letter
+    categories = list(liste)[j:k] 
+    loop_to_plot2 (liste=liste, j=j, k=k)
 
 ##############################################################################
-# predict presence of verbal perseveration early postop by applying different supervised learning analysis
-from sklearn.svm import SVC
-from sklearn.model_selection import (cross_val_score, KFold, GridSearchCV)
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
+# PREDICT presence ofsevere intraoperative verbal perseverations (siVP) 
+# by applying different supervised learning analysis
 
 
-#select disconnectome data : here we chose the first method of averaging
 
-X = ANTS_ratio.iloc[:, 0:46]
-y = behavior.iloc[:, 3]
+#select disconnectome data : 
 
-# remove subject with nan value from both datasets (here the second line)
-X = X.drop(X.index[1])
-y = y.drop(y.index[1])
+X = EF
+y = EF["siVP"]
+
 
 ####SVC rbf
 nested_score_rbf=np.zeros(4)
@@ -265,7 +348,8 @@ outer_cv = KFold(n_splits=4, shuffle=True, random_state=0)
 # Nested CV with parameter optimization
 clf = GridSearchCV(estimator=svm, param_grid=p_grid, cv=inner_cv)
 nested_score_rbf = cross_val_score(clf, X=X, y=y, cv=outer_cv)
-print('Mean score of {:6f} with std. dev. of {:6f}.'.format(nested_score_rbf.mean(),nested_score_rbf.std()))
+print('Mean score of {:6f} with std. dev. of {:6f}.'
+      .format(nested_score_rbf.mean(),nested_score_rbf.std()))
 
 ###SVC linear
 nested_score_linear=np.zeros(4)
@@ -279,12 +363,14 @@ outer_cv = KFold(n_splits=4, shuffle=True, random_state=0)
 # Nested CV with parameter optimization
 clf = GridSearchCV(estimator=svm, param_grid=p_grid, cv=inner_cv)
 nested_score_linear = cross_val_score(clf, X=X, y=y, cv=outer_cv)
-print('Mean score of of {:6f} with std. dev. of {:6f}.'.format(nested_score_linear.mean(),nested_score_linear.std()))
+print('Mean score of of {:6f} with std. dev. of {:6f}.'
+      .format(nested_score_linear.mean(),nested_score_linear.std()))
 
 ###random forest
 nested_score_rf = np.zeros(4)
 # Set up possible values of parameters to optimize over
-p_grid = {"max_depth": [2, 4, 6, 8], "n_estimators" : [2,4,6,8,10,12,14,16,18,20]}
+p_grid = {"max_depth": [2, 4, 6, 8], "n_estimators" : 
+    [2,4,6,8,10,12,14,16,18,20]}
 rf= RandomForestClassifier(random_state=0)
 
 # crossvalidation
@@ -293,7 +379,8 @@ outer_cv = KFold(n_splits=4, shuffle=True, random_state=0)
 # Nested CV with parameter optimization
 clf = GridSearchCV(estimator=rf, param_grid=p_grid, cv=inner_cv)
 nested_score_rf = cross_val_score(clf, X=X, y=y, cv=outer_cv)
-print('Mean score of of {:6f} with std. dev. of {:6f}.'.format(nested_score_rf.mean(),nested_score_rf.std()))
+print('Mean score of of {:6f} with std. dev. of {:6f}.'
+      .format(nested_score_rf.mean(),nested_score_rf.std()))
 
 ###Logistic Regression
 nested_score_lr = np.zeros(4)
@@ -307,4 +394,33 @@ outer_cv = KFold(n_splits=4, shuffle=True, random_state=0)
 # Nested CV with parameter optimization
 clf = GridSearchCV(estimator=lr, param_grid=p_grid, cv=inner_cv)
 nested_score_lr = cross_val_score(clf, X=X, y=y, cv=outer_cv)
-print('Mean score of of {:6f} with std. dev. of {:6f}.'.format(nested_score_lr.mean(),nested_score_lr.std()))
+print('Mean score of of {:6f} with std. dev. of {:6f}.'
+      .format(nested_score_lr.mean(),nested_score_lr.std()))
+
+#####TREE
+
+X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=0)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+tree.plot_tree(clf)
+clf.score(X_test, y_test)
+
+r = export_text(clf,X.columns)
+print(r)
+
+#NESTED CROSSVALIDATION
+nested_score_rf = np.zeros(4)
+# Set up possible values of parameters to optimize over
+p_grid = {"max_depth": [2, 4, 6, 8], "min_samples_leaf" : [1,2,3,4,5]}
+rf= tree.DecisionTreeClassifier(random_state=0)
+
+# crossvalidation
+inner_cv = KFold(n_splits=4, shuffle=True, random_state=0)
+outer_cv = KFold(n_splits=4, shuffle=True, random_state=0)
+# Nested CV with parameter optimization
+clf = GridSearchCV(estimator=rf, param_grid=p_grid, cv=inner_cv)
+nested_score_tree = cross_val_score(clf, X=X, y=y, cv=outer_cv)
+print('Mean score of of {:6f} with std. dev. of {:6f}.'.format(nested_score_rf.mean(),nested_score_rf.std()))
+r = export_text(clf,X.columns)
+print(r)   
