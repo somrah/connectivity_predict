00d3c92a0e841c9cfbdebcdc1755f2db15f07fcf
bthirion <bertrand.thirion@inria.fr>
2021-02-11 16:15:47 +0100

    fixing table reading

--- population_analysis.py
+++ population_analysis.py
@@ -11,23 +11,22 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score, ShuffleSplit
 
 
-
-
+n_permutations = 1000
+scoring = 'neg_mean_squared_error'
 
 df = pd.read_csv('liste_patients_gliome_final_total.csv', index_col=0)
 
 # Drop a column with only Nans
 df.drop(labels='CorticoThalamic_4', axis=1, inplace=True)
 
-networks = df.columns[:-2]
-X = df.values[:, :-2]
-y = df.values[:, -1]
-
+networks = df.columns[:-3]
+X = df.values[:, :-3]
+y = df.values[:, -2] + 0.01 * df.values[:, -1] * np.sign(df.values[:, -2])
 plt.figure()
 plt.hist(y, bins=10)
 
 # define classifier
-clf = RandomForestRegressor(max_depth=2)  # max_depth=5, max_features=1
+clf = RandomForestRegressor(max_depth=2)  # max_depth=2, max_features=1
 
 #define cross_validation scheme
 cv = ShuffleSplit(n_splits=100, test_size=.25, random_state=0)
@@ -37,29 +36,31 @@ r2_ = cross_val_score(clf, X, y, cv=cv,n_jobs=5)
 print(r2_.mean())
 
 mae_ = cross_val_score(clf, X, y, cv=cv, n_jobs=5,
-                       scoring='neg_mean_squared_error')
+                       scoring=scoring)
 mmae = mae_.mean()
-print(mmae)
+print('rf:', mmae)
 
 # compare to permutation distribution
 maes = []
 y_ = y.copy()
-for _ in range(100):
+for _ in range(n_permutations):
     np.random.shuffle(y_)
     maes_ = cross_val_score(clf, X, y_, cv=cv, n_jobs=5,
-                       scoring='neg_mean_squared_error')
+                       scoring=scoring)
     maes.append(np.mean(maes_))
 
 plt.figure()
 plt.hist(maes, bins=10)
 plt.plot(mmae, 0, '*r')
+print(np.sum(maes > mmae))
 
 # Try OOB errors on Random Forests
+"""
 clf = RandomForestRegressor(max_depth=2, oob_score=True)
 clf.fit(X, y)
 oob_score_ref = clf.oob_score_
 oob_scores = []
-for _ in range(100):
+for _ in range(n_permutations):
     np.random.shuffle(y_)
     clf.fit(X, y_)
     oob_scores.append(clf.oob_score_)
@@ -67,20 +68,32 @@ for _ in range(100):
 plt.figure()
 plt.hist(oob_scores, bins=10)
 plt.plot(oob_score_ref, 0, '*r')
+print('rf, oob: ', oob_score_ref, np.sum(oob_score_ref > oob_scores))
+"""
 
 # attempt with Ridge regression
 from sklearn.linear_model import RidgeCV
 clf = RidgeCV()
 mae_ = cross_val_score(clf, X, y, cv=cv, n_jobs=5,
-                       scoring='neg_mean_squared_error')
+                       scoring=scoring)
 mmae = mae_.mean()
-print(mmae)
+print('ridge:', mmae)
+for _ in range(n_permutations):
+    np.random.shuffle(y_)
+    maes_ = cross_val_score(clf, X, y_, cv=cv, n_jobs=5,
+                       scoring=scoring)
+    maes.append(np.mean(maes_))
+print(np.sum(maes > mmae))
+
+plt.figure()
+plt.hist(maes, bins=10)
+plt.plot(mmae, 0, '*r')
 
 # attempt with GBT
 from sklearn.ensemble import GradientBoostingRegressor
 clf = GradientBoostingRegressor()
 mae_ = cross_val_score(clf, X, y, cv=cv, n_jobs=5,
-                       scoring='neg_mean_squared_error')
+                       scoring=scoring)
 mmae = mae_.mean()
 print(mmae)
 
